8df307de1259376cea71a9e0738b98272a773042
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-11 17:28:42 -0300

    Implementação da Interface e do Componente FileUpload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import FileUpload from './Components/FileUpload/file-upload.component';
 
 const App = () => {
   return (
     <div className="app" data-testid="app">
+      <div className= "app-header">Validação de Equipamentos Elétricos</div>
       <div className="app-body" data-testid="app-body">
-        
+        <FileUpload></FileUpload>
       </div>
     </div>
   );
---
+++ src/Components/index.ts
@@ -0,0 +1,1 @@
+export *  from './FileUpload/file-upload.component';
