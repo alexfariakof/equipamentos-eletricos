4d084c6fd17e1ef5fb8ef09f3cdd4f01021ea955
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-11 15:23:29 -0300

    Initial commit feature/Create-Interface-to-Upload-Files

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import './App.css';
 const App = () => {
   return (
     <div className="app" data-testid="app">
-      <div className="app-body" data-testid="app-body">Equipamentos Elétricos</div>
+      <div className="app-body" data-testid="app-body">
+        
+      </div>
     </div>
   );
 }
