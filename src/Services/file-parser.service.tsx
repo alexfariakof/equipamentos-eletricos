2c1c074ff609ae34bf21dd58881adb0137ec17cf
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-11 22:00:03 -0300

    Implementação de um Serviço para Converter o Arquivo em um Array com os Tipos de Equipamentos

--- src/Services/file-parser.service.tsx
+++ src/Services/file-parser.service.tsx
@@ -1,3 +1,60 @@
-const FileParserService = () => {}
+import Bar from "../Domain/Entities/Bar";
+import Line from "../Domain/Entities/Line";
+import ShuntBar from "../Domain/Entities/ShuntBar";
+import ShuntLine from "../Domain/Entities/ShuntLine";
+
+class FileParserService {
+  static parseFile(fileContent: string): any[] {
+    const lines = fileContent.split("\n");
+    const parsedData: any[] = [];
+
+    lines.forEach((line) => {
+      const trimmedLine = line.trim();
+
+      if (trimmedLine) {
+        const [type, ...data] = trimmedLine
+          .split(",")
+          .map((part) => part.trim());
+
+        if (type && data.length > 0) {
+          const parsedItem: any = {};
+
+          switch (type) {
+            case "B":
+              parsedItem.bar = new Bar(parseInt(data[0], 10));
+              break;
+            case "L":
+              parsedItem.line = new Line(
+                new Bar(parseInt(data[0], 10)),
+                new Bar(parseInt(data[1], 10)),
+                parseInt(data[2], 10)
+              );
+              break;
+            case "SL":
+              parsedItem.shuntLine = new ShuntLine(
+                new Bar(parseInt(data[0], 10)),
+                new Bar(parseInt(data[1], 10)),
+                parseInt(data[2], 10),
+                parseInt(data[3], 10)
+              );
+              break;
+            case "SB":
+              parsedItem.shuntBar = new ShuntBar(
+                new Bar(parseInt(data[0], 10)),
+                parseInt(data[1], 10)
+              );
+              break;
+            default:
+              throw new Error(`Tipo desconhecido: ${type}`);
+          }
+
+          parsedData.push(parsedItem);
+        }
+      }
+    });
+
+    return parsedData;
+  }
+}
 
 export default FileParserService;
