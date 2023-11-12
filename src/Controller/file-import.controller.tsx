b362fb881f4ce50b686292f03b65d0df5f93f251
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-11 21:58:53 -0300

    Implementação do Controller que recebe o Arquivo para Validação

--- src/Controller/file-import.controller.tsx
+++ src/Controller/file-import.controller.tsx
@@ -1,3 +1,44 @@
+import TopologyValidationBussiness from '../Bussiness/topology-validation.bussiness';
+import FileParserService from '../Services/file-parser.service';
 
-const FileImportController = () => {}
+class FileImportController  {
+
+   postFileToValidate = async (file: File) : Promise<{ message: string; }> => {  
+    try {
+      
+      const fileContent = await this.readFileAsync(file);
+      const parsedData = FileParserService.parseFile(fileContent);
+      // Validar topologia
+      const validationResult = TopologyValidationBussiness.validateTopology(parsedData);
+      if (validationResult.status){
+        return { message: validationResult.message };
+      }
+      else{
+        return { message: validationResult.message };
+      }
+        
+
+    } catch (error: any) {
+
+      return { message: error.message };
+    }
+  }
+
+  private readFileAsync(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        if (event.target) {
+          resolve(event.target.result as string);
+        } else {
+          reject(new Error('Erro ao ler o conteúdo do arquivo.'));
+        }
+      };
+      reader.onerror = (event) => {
+        reject(new Error('Erro ao ler o conteúdo do arquivo.'));
+      };
+      reader.readAsText(file);
+    });  
+  }
+}
 export default FileImportController;
