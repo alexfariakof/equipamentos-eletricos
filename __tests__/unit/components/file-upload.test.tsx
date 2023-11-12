938825a6078b602a06edf03d803db1f9f505d8c1
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-11 23:35:49 -0300

    Implementação de Testes Unitários Componente FileUpload

--- __tests__/unit/components/file-upload.test.tsx
+++ __tests__/unit/components/file-upload.test.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import FileUpload from '../../../src/Components/FileUpload/file-upload.component';
+import FileImportController from '../../../src/Controller/file-import.controller';
+
+jest.mock('../../../src/Controller/file-import.controller', () => {
+  return {
+    __esModule: true,
+    default: jest.fn(),
+  };
+});
 
 describe("Unit Test FileUpload Component", () => {
 
-  test('FileUpload should component work', () => {
+  beforeEach(() => {
+    
+    jest.clearAllMocks();
+  });
+
+  test('Should Work with a Valid File and show Message', async () => {
     // Arrange & Act
     render(<FileUpload />);
-    const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => {});
+    const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => { });
+    FileImportController.prototype.postFileToValidate = jest.fn().mockResolvedValue({ message: true });
 
     // Simula a seleção de um arquivo
     const fileInput = screen.getByTestId('fileUpload');
@@ -19,12 +33,66 @@ describe("Unit Test FileUpload Component", () => {
     fireEvent.click(uploadButton);
 
     // Assert
+    await waitFor(() => {
+      expect(FileImportController.prototype.postFileToValidate).toHaveBeenCalledWith(file);      
+    });
     expect(alertSpy).toHaveBeenCalled();
     expect(alertSpy).toHaveBeenCalledWith('Upload bem-sucedido!');
     alertSpy.mockRestore();
   });
 
-  test('FileUploaded with wrong type should show message', () => {
+
+  test('Should Show Message for InValid File', async () => {
+    // Arrange & Act
+    render(<FileUpload />);
+    const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => { });
+    FileImportController.prototype.postFileToValidate = jest.fn().mockResolvedValue({ message: false });
+
+    // Simula a seleção de um arquivo
+    const fileInput = screen.getByTestId('fileUpload');
+    const file = new File([''], '../../../.files_to_validate/arquivoE.CSV', { type: 'text/csv' });
+    fireEvent.change(fileInput, { target: { files: [file] } });
+
+    // Simula o clique no botão de upload
+    const uploadButton = screen.getByTestId('btnValidar');
+    fireEvent.click(uploadButton);
+
+    // Assert
+    await waitFor(() => {
+      expect(FileImportController.prototype.postFileToValidate).toHaveBeenCalledWith(file);      
+    });
+    expect(alertSpy).toHaveBeenCalled();
+    expect(alertSpy).not.toContainEqual('Upload bem-sucedido!');
+    alertSpy.mockRestore();
+  });
+
+
+  test('Should Throws Error and Show Message', async () => {
+    // Arrange & Act
+    render(<FileUpload />);
+    const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => { });
+    FileImportController.prototype.postFileToValidate = jest.fn().mockRejectedValue({ message: false });
+
+    // Simula a seleção de um arquivo
+    const fileInput = screen.getByTestId('fileUpload');
+    const file = new File([''], '../../../.files_to_validate/arquivoC.txt', { type: 'text/txt' });
+    fireEvent.change(fileInput, { target: { files: [file] } });
+
+    // Simula o clique no botão de upload
+    const uploadButton = screen.getByTestId('btnValidar');
+    fireEvent.click(uploadButton);
+
+    // Assert
+    await waitFor(() => {
+      expect(FileImportController.prototype.postFileToValidate).toHaveBeenCalledWith(file);      
+    });
+    expect(alertSpy).toHaveBeenCalled();
+    expect(alertSpy).not.toContainEqual('Upload bem-sucedido!');
+    alertSpy.mockRestore();
+  });
+
+
+  test('Should Show Message when Entry with Wrong Type or Empty File', () => {
     // Arrange & Act
     render(<FileUpload />);
     const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => {});
